c2e03686e1053c3dd88a4db172200700d0d8a149
hiven <james@hiven.co.uk>
2016-01-25 15:43:01 +0000

    Update forms.py

--- project/home/forms.py
+++ project/home/forms.py
@@ -5,5 +5,9 @@ from wtforms.validators import DataRequired, Length
 
 class MessageForm(Form):
     title = TextField('Title', validators=[DataRequired()])
+    company = TextField('Company', validators=[DataRequired()])
+    companyurl = TextField('Companyurl', validators=[DataRequired()])
+    location = TextField('Location', validators=[DataRequired()])
+    salary = TextField('Salary', validators=[DataRequired()])
     description = TextField(
         'Description', validators=[DataRequired(), Length(max=140)])
